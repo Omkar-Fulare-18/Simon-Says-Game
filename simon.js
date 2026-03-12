a42430cf5dad462b15fa86204599f4139bfb468b
Omkar Fulare <omkarfularework@gmail.com>
2026-03-12 12:51:14 +0530

    Initial Commit

---
+++ simon.js
@@ -0,0 +1,89 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red", "green", "yellow", "blue"];
+
+let started = false;
+let level = 0;
+let h2 = document.querySelector("h2");
+let body = document.querySelector("body")
+let h3 = document.querySelector("h3");
+
+
+document.addEventListener("keydown", function() {
+    if (started == false) {
+        console.log("game started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 300);
+}
+
+function userFlash(btn) {
+    btn.classList.add("userFlash");
+    setTimeout(function () {
+        btn.classList.remove("userFlash");
+    }, 300);
+}
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`; 
+
+    let ranIdx = Math.floor(Math.random() * 4);
+    let ranColor = btns[ranIdx];
+    let ranBtn = document.querySelector(`.${ranColor}`);
+    gameSeq.push(ranColor);
+    console.log("Game Seq = ",gameSeq);
+    gameFlash(ranBtn);
+}
+
+function checkAns(idx) {
+    if (userSeq[idx] === gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        reset();
+    }
+}
+
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    // console.log("User Seq = ",userSeq);
+
+    checkAns(userSeq.length - 1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    h3.innerText = `Highest Score is ${level}!`
+    h2.innerHTML = `Game Over! Your score was ${level}! <br> Press any key to start the game again`;
+    started = false; 
+    level = 0;
+    gameSeq = [];
+    body.classList.add("gameOver");
+    setTimeout(function () {
+        body.classList.remove("gameOver");
+    }, 300);
+}
+
+if(level >= level) {
+    level = level;
+}
